e6a2c326dd50ee9e6020d09f800ebe1e8930b7c5
sazzad_saju <saju.cse.hstu@gmail.com>
2025-06-25 09:57:48 +0600

    ui design

--- cartrader/app.vue
+++ cartrader/app.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- NAVBAR -->
+    <header
+      class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b-4 border-white bg-white shadow-md px-4"
+      style="height: 5rem;">
+      <a class="text-3xl font-mono" href="">Car Trader</a>
+    </header>
+    <!-- NAVBAR -->
+
+   
   </div>
 </template>
---
+++ cartrader/html/carDetail.vue
@@ -0,0 +1,56 @@
+<template>
+     <!-- CAR DETAIL PAGE -->
+    <div class="mx-auto mt-4 mt-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
+      <div>
+        <!-- CAR HERO -->
+
+        <div class="mt-10">
+          <img src="/img/car-details-1.png" class="w-full" alt="">
+          <!-- <div class="absolute inset-0 flex flex-col justify-end p-8"> -->
+          <h1 class="mt-10 text-4xl">Ferrari 488 GTB</h1>
+          <div class="text-slate-500 flex text-lg mt-3 border-b border-gray-200 pb-5 justify-between">
+            <div class="flex">
+              <p class="mr-2">5 seats</p>
+              <p class="mr-2">|</p>
+              <p class="mr-2">67,444 miles</p>
+            </div>
+            <div>
+              <p class="font-bold text-2xl">$39,555</p>
+            </div>
+          </div>
+
+          <!-- </div> -->
+        </div>
+      </div>
+      <!-- CAR HERO -->
+      <!-- CAR ATTRIBUTES -->
+      <div class="mr-10 mt-5 border-b border-gray-200 pb-5">
+        <div class="flex text-lg mt-2">
+          <p class="rounded text-lime-800 mr-3">✓</p>
+          <p>Leather Interior</p>
+        </div>
+      </div>
+      <!-- CAR ATTRIBUTES -->
+       <!-- CAR DESCRIPTION -->
+        <div class="mt-5">
+          <p class="mb-10">
+            This Ferrari 488 GTB is a stunning example of Italian engineering and design. With its powerful V8 engine and sleek lines, it's sure to turn heads wherever you go.
+          </p>
+        </div>
+      <!-- CAR DESCRIPTION -->
+       <!-- CAR CONTACT -->
+        <div class="mt-10 w-[678px]">
+          <div class="flex justify-between gap-4">
+            <input type="text" class="border border-gray-200 p-1 flex-1" placeholder="Name">
+            <input type="text" class="border border-gray-200 p-1 flex-1" placeholder="Email">
+            <input type="text" class="border border-gray-200 p-1 flex-1" placeholder="Phone">
+          </div>
+          <div class="flex mt-4">
+            <textarea class="border border-gray-200 p-1 w-full" placeholder="Message"></textarea>
+          </div>
+         <button class="bg-blue-400 text-white px-10 py-3 rounded mt-4">Submit</button>
+        </div>
+        <!-- CAR CONTACT -->
+      </div>
+    <!-- CAR DETAIL PAGE -->
+</template>
