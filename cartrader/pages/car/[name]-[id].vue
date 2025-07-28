b2538f9380b75acd244c12465db6898f5024b13b
sazzad-saju <saju.cse.hstu@gmail.com>
2025-07-28 18:17:15 +0600

    Add: define page layouts

--- cartrader/pages/car/[name]-[id].vue
+++ cartrader/pages/car/[name]-[id].vue
@@ -1,31 +1,20 @@
 <script setup>
 const route = useRoute();
+const { toTitleCase } = useUtilities();
 useHead({
-  title: toTitleCase(route.params.name) 
+  title: toTitleCase(route.params.name)
 })
 
-function toTitleCase(str) {
-    return str.replace(/\w\S*/g, function (txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
-}
+definePageMeta({
+  layout: 'custom',
+})
+
 </script>
 <template>
-    <div>
-        <NavBar />
-        <div class="mx-auto mt-4 mt-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
-        
-        <CarDetailHero />
-      
-      <!-- CAR ATTRIBUTES -->
-      <CarDetailAttributes />
-      <!-- CAR ATTRIBUTES -->
-       <!-- CAR DESCRIPTION -->
+  <div>
+    <CarDetailHero />
+    <CarDetailAttributes />
     <CarDetailDescription />
-      <!-- CAR DESCRIPTION -->
-       <!-- CAR CONTACT -->
-        <CarDetailContact />
-        <!-- CAR CONTACT -->
-      </div>
-    </div>
+    <CarDetailContact />
+  </div>
 </template>
--- cartrader/pages/city/[city]/car.vue
+++ cartrader/pages/city/[city]/car.vue
@@ -1,26 +1,22 @@
 <script setup>
 const route = useRoute();
+const { toTitleCase } = useUtilities();
 useHead({
     title: ` ${route.params.make ? toTitleCase(route.params.make) : 'Cars'} in ${toTitleCase(route.params.city)}`
 })
 
-function toTitleCase(str) {
-    return str.replace(/\w\S*/g, function (txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
-}
+definePageMeta({
+    layout: 'custom',
+})
 
 </script>
 
 <template>
     <div>
-        <NavBar />
-        <div class="mx-auto mt-4 mt-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
-            <div class="mt-32 flex">
-                <CarSideBar />
+        <div class="mt-32 flex">
+            <CarSideBar />
 
-                <NuxtPage />
-            </div>
+            <NuxtPage />
         </div>
     </div>
 </template>
