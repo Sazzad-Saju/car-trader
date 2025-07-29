6c12b84acb040e905f241943b3eee5712b1edf5e
sazzad-saju <saju.cse.hstu@gmail.com>
2025-07-29 19:06:38 +0600

    Add: dynamic description

--- cartrader/components/Car/Detail/Hero.vue
+++ cartrader/components/Car/Detail/Hero.vue
@@ -1,16 +1,21 @@
+<script setup>
+const props = defineProps({
+  car: Object,
+});
+</script>
 <template>
     <div class="mt-10">
-          <img src="/img/car-details-1.png" class="w-full" alt="">
+          <img :src="car.url" class="w-full" alt="">
           <!-- <div class="absolute inset-0 flex flex-col justify-end p-8"> -->
-          <h1 class="mt-10 text-4xl">Ferrari 488 GTB</h1>
+          <h1 class="mt-10 text-4xl">{{ car.name  }}</h1>
           <div class="text-slate-500 flex text-lg mt-3 border-b border-gray-200 pb-5 justify-between">
             <div class="flex">
-              <p class="mr-2">5 seats</p>
+              <p class="mr-2">{{car.seats}} seats</p>
               <p class="mr-2">|</p>
-              <p class="mr-2">67,444 miles</p>
+              <p class="mr-2">{{ car.miles }} miles</p>
             </div>
             <div>
-              <p class="font-bold text-2xl">$39,555</p>
+              <p class="font-bold text-2xl">${{ car.price }}</p>
             </div>
           </div>
         </div>
