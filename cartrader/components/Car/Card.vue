239b6b949205859224f9d7dd6d46d924efc6557e
sazzad-saju <saju.cse.hstu@gmail.com>
2025-07-29 18:38:54 +0600

    client+Server error

--- cartrader/components/Car/Card.vue
+++ cartrader/components/Car/Card.vue
@@ -1,16 +1,22 @@
+<script setup>
+const props = defineProps({
+  car: Object,
+});
+</script>
+
 <template>
-    <div class="shadow border border-gray-200 w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
-            <div class="flex h-full">
-              <img src="/img/car-1.jpg" alt="" class="w-[300px] h-full">
-              <div class="p-4 flex flex-col">
-                <div>
-                  <h1 class="text-2xl text-blue-700">
-                    Ferrari 488 GTB
-                  </h1>
-                  <p class="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                </div>
-                <h1 class="mt-auto text-xl">$39,555</h1>
-              </div>
-            </div>
-          </div>
+  <div class="shadow border border-gray-200 w-full overflow-hidden mb-5 cursor-pointer h-[200px]" @click="navigateTo(`/car/${car.name}-${car.id}`)">
+    <div class="flex h-full">
+      <img :src="car.url" alt="" class="w-[300px] h-full">
+      <div class="p-4 flex flex-col">
+        <div>
+          <h1 class="text-2xl text-blue-700">
+            {{ car.name }}
+          </h1>
+          <p class="text-gray-700">{{ car.description }}</p>
+        </div>
+        <h1 class="mt-auto text-xl">${{ car.price }}</h1>
+      </div>
+    </div>
+  </div>
 </template>
---
+++ cartrader/error.vue
@@ -0,0 +1,16 @@
+<script setup>
+const error = useError();
+const handleError = () => {
+    navigateTo('/');
+}
+
+</script>
+<template>
+  <div class="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center bg-white">
+    <h1 class="text-9xl">{{ error.statusCode }}</h1>
+    <p class="mt-7 text-4xl">{{ error.message }}</p>
+    <button @click="handleError" class="text-xl bg-blue-400 px-10 py-6 text-white rounded mt-5">
+      Go Back
+    </button>
+  </div>
+</template>
