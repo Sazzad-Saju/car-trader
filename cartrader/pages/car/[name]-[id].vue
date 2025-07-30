6e8c69635881cfc7c18363aa1c348a3bacffb949
sazzad-saju <saju.cse.hstu@gmail.com>
2025-07-30 18:53:11 +0600

    Add: handle ssr error and csr error

--- cartrader/pages/car/[name]-[id].vue
+++ cartrader/pages/car/[name]-[id].vue
@@ -12,13 +12,20 @@ const car = computed(() => {
   })
 })
 
+if(!car.value){
+  throw createError({
+    statusCode: 404,
+    message: `Car with ID ${route.params.id} does not exist`
+  })
+}
+
 definePageMeta({
   layout: 'custom',
 })
 
 </script>
 <template>
-  <div v-if="car">
+  <div>
     <CarDetailHero :car="car"/>
     <CarDetailAttributes :features="car.features" />
     <CarDetailDescription :description="car.description"/>
