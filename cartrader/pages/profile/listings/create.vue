c8812cfac07f040b083bae0a3b72ae21949f641b
Sazzad-Saju <sazzadsaju17@gmail.com>
2025-08-24 02:27:35 +0600

    Add: try supabase oAuth

--- cartrader/pages/profile/listings/create.vue
+++ cartrader/pages/profile/listings/create.vue
@@ -1,6 +1,7 @@
 <script setup>
 definePageMeta({
     layout: "custom",
+    middleware: ["auth"],
 });
 const { makes } = useCars();
 const info = useState('adInfo', () => {
@@ -67,7 +68,6 @@ const inputs = [
         <div class="mt-24">
             <h1 class="text-6xl">Create a New Listing</h1>
         </div>
-        {{info}}
         <div class="shadow rounded p-3 mt-5 flex flex-wrap justify-between">
             <CarAdSelect title="Make *" :options="makes" name="make" @change-input="onChangeInput" />
             <CarAdInput v-for="input in inputs" :key="input.id" :title="input.title" :name="input.name" :placeholder="input.placeholder" @change-input="onChangeInput" />
