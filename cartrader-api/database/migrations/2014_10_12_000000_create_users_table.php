d372f00b5bf4f8425fea8e0fe54fe7a4bfb252bf
sazzad-saju <saju.cse.hstu@gmail.com>
2025-08-31 14:27:49 +0600

    Add: fix table issue

--- cartrader-api/database/migrations/2014_10_12_000000_create_users_table.php
+++ cartrader-api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('google_id')->nullable();
-            $table->string('avatar')->nullable();
+            $table->string('avatar', 512)->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
