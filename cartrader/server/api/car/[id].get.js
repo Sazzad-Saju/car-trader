e4b682813a1f32be406c3b12850cf51a89cc1c81
Sazzad-Saju <sazzadsaju17@gmail.com>
2025-09-26 17:44:28 +0600

    Add: Refactor other api + Remove laravel as api

--- cartrader/server/api/car/[id].get.js
+++ cartrader/server/api/car/[id].get.js
@@ -1,12 +1,16 @@
-import cars from "@/data/cars.json";
+import { prisma } from "~/server/utils/prisma";
 
  export default defineEventHandler(async (event) => {
     const { id } = event.context.params;
 
-    const car = cars.find(c => c.id === parseInt(id));
+    const car = await prisma.car.findUnique({
+        where: {
+            id: parseInt(id)
+        }
+    });
 
     if(!car) {
-        throw createError({
+        throw createError({ 
             statusCode: 404,
             statusMessage: `Car with id of ${id} not found`
         });
--- cartrader/server/api/cars/[city].get.js
+++ cartrader/server/api/cars/[city].get.js
@@ -1,25 +1,33 @@
-import cars from "@/data/cars.json";
+import { prisma } from "~/server/utils/prisma";
 
 export default defineEventHandler(async (event) => { 
     const { city } = event.context.params;
 
     const { make, minPrice, maxPrice } = getQuery(event);
-
-    let filteredCars = cars;
-
-    filteredCars = filteredCars.filter(car => car.city.toLowerCase() === city.toLowerCase());
+    
+    const filters = {
+        city: city.toLowerCase()
+    }
 
     if(make) {
-        filteredCars = filteredCars.filter(car => car.make.toLowerCase() === make.toLowerCase());
+        filters.make = make
+    }
+    
+    if(minPrice || maxPrice) {
+        filters.price = {}
     }
 
     if(minPrice) {
-        filteredCars = filteredCars.filter(car => car.price >= parseInt(minPrice));
+        filters.price.gte = parseInt(minPrice);
     }
 
     if(maxPrice) {
-        filteredCars = filteredCars.filter(car => car.price <= parseInt(maxPrice));
+        filters.price.lte = parseInt(maxPrice);
     }
-
-    return filteredCars;
+    
+    // return filters;
+    
+    return prisma.car.findMany({
+        where: filters
+    });
 });
